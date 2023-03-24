06df29e34ee0e9d2b8c6306a4fd8dbd01d8734d4
Andres Londono <aflondono@users.noreply.github.com>
2023-03-24 22:35:36 +1000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Andres Londono on 24/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
